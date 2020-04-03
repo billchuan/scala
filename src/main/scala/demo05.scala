02a84ac3df6a182d807950ef5f572d8516feb910
billchuan <1767618766@qq.com>
2020-04-03 09:45:38 +0800

    4月3日学习更新

--- src/main/scala/demo05.scala
+++ src/main/scala/demo05.scala
@@ -1,11 +1,16 @@
 //4月3日一、二节课学习
 object demo05 {
-  //惰性函数
+  //惰性函数，延迟加载(首次不执行再次调用执行)
   def main(args: Array[String]): Unit = {
     lazy val res = sum(10, 20)
     println("---------------------")
     println("res="+res)
+
   }
+
+
+
+
 
   def sum(a: Int, b: Int): Int = {
     println("sum()函数执行了.................")
