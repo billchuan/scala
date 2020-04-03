824a8a5cba7f4fb47218ad6db88ed06db549f6fd
billchuan <1767618766@qq.com>
2020-04-03 08:59:20 +0800

    4月3日学习更新

--- src/main/scala/demo05.scala
+++ src/main/scala/demo05.scala
@@ -1,10 +1,15 @@
 //4月3日一、二节课学习
 object demo05 {
+  //惰性函数
+  def main(args: Array[String]): Unit = {
+    lazy val res = sum(10, 20)
+    println("---------------------")
+  }
 
-
-
-
-
+  def sum(a: Int, b: Int): Int = {
+    println("sum()函数执行了.................")
+    return a + b
+  }
 
 
 }
