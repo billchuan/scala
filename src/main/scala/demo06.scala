c64a3425e46dad76c89acfa42df3691fcfb47422
billchuan <1767618766@qq.com>
2020-04-10 09:53:55 +0800

    4月10日学习更新

--- src/main/scala/demo06.scala
+++ src/main/scala/demo06.scala
@@ -1,5 +1,27 @@
-//4月3日三、四节课学习
+//4月10日一、二节课学习
+//类的定义练习
 object demo06 {
+  def main(args: Array[String]): Unit = {
+    val cat = new cat
+    cat.name = "小白"
+    cat.age = 3
+    cat.color = "白色"
 
+
+
+    println("小猫的信息如下，名字:" + cat.name + "\t颜色:" + cat.color + "\t年龄:" + cat.age)
+  }
+}
+
+
+class cat {
+  var name: String = " "
+  var age: Int = _
+  var color: String = _
+  var dog = null
+
+}
+
+class dog {
 
 }
