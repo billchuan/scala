5a33ebc4b5f8f6a2406ac3b7b7d5b01e69824a2d
billchuan <1767618766@qq.com>
2020-04-17 09:49:29 +0800

    4月17日学习更新

--- src/main/scala/demo08.scala
+++ src/main/scala/demo08.scala
@@ -2,15 +2,23 @@ import scala.beans.BeanProperty
 
 object demo08 {
   def main(args: Array[String]): Unit = {
-    val car1 =new car
-    car1.name="BMW"
+    val car1 = new car1
+    car1.name = "BMW"
     println(car1.name)
     println("--------------------")
+    val car2 = new car2
+    car2.name = "aaa"
 
+    car2.setName("bbb")
+    println(car2.getName)
   }
 
 }
 
-class car {
-  @BeanProperty var name: String = null
+class car1 {
+  var name: String = null
+}
+
+class car2 {
+  @BeanProperty var name: String = _
 }
