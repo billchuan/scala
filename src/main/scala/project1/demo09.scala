f542245a726e45821d2355dec3c03c4ef2c85a08
billchuan <1767618766@qq.com>
2020-04-24 10:35:24 +0800

    4月24日学习更新

--- src/main/scala/project1/demo09.scala
+++ src/main/scala/project1/demo09.scala
@@ -2,10 +2,10 @@ package project1
 
 object demo09 {
   def main(args: Array[String]): Unit = {
-    val cat1=new xm.cat
-    println(cat1)
-    val cat2=new xq.cat
-    println(cat2)
+    //val cat1=new xm.cat
+    //println(cat1)
+    //val cat2=new xq.cat
+    //println(cat2)
 
   }
 }
--- src/main/scala/project1/demo10.scala
+++ src/main/scala/project1/demo10.scala
@@ -1,5 +1,26 @@
+//4月24日学习
+
 package project1
 
 object demo10 {
-
+  def main(args: Array[String]): Unit = {
+    val emp = new emp
+    emp.printname()
+  }
 }
+
+class person {
+  var name: String = _
+
+  def printname() = {
+    print("person printname()" + name)
+  }
+}
+
+
+class emp extends person {
+  override def printname(): Unit = {
+    println("emp printname()" + name)
+  }
+}
+
